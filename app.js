69d49b20a614a810888a92f595b52e7e947221da
C4P10 Can <85010376+C4P10@users.noreply.github.com>
2022-02-03 21:44:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+// set the dimensions and margins of the graph
+const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#my_dataviz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("https://c4p10.github.io/D3CSVStore/data.csv", function (data) {
+
+    // X axis
+    const x = d3.scaleBand()
+        //where x axis starts, how wide it is     
+        .range([0, width])
+        //data that is displayed on the axis
+        .domain(data.map(d => d.Country))
+        //space between bars
+        .padding(0.2);
+    svg.append("g")
+        //height is how far it is from the top on the y axis    
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0) rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+        .domain([0, 13000])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Bars
+    svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return x(d.Country); })
+        .attr("y", function (d) { return y(d.Value); })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) { return height - y(d.Value); })
+        .attr("fill", "#69b3a2")
+});
